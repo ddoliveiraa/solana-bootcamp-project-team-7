ee4cfbcf11b5a48a602a9f2cb47f46c179cb46d9
Charlie Torres <charlie.torres@arcadis.com>
2024-04-30 18:25:08 +0000

    image upload into create event form

--- blockchain-ticketing-system-frontend/src/app/image-upload/component.ts
+++ blockchain-ticketing-system-frontend/src/app/image-upload/component.ts
@@ -1,34 +1,12 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { create } from 'ipfs-http-client';
-=======
 import { create } from '@web3-storage/w3up-client';
 import { Buffer } from 'buffer';
->>>>>>> d3ba1c52ddaa8f4be03abf52578bd8234bf62114
 
 @Component({
     selector: 'app-image-upload',
     templateUrl: './image-upload.component.html',
-    // styleUrls: ['./image-upload.component.css']
+    styleUrls: ['./image-upload.component.scss']
 })
-<<<<<<< HEAD
-export class ImageUploadComponent {
-    async processFile(imageInput: any): Promise<void> {
-        const file: File = imageInput.files[0];
-        if (file) {
-            try {
-                // Initialize client
-                const client = create({ host: 'ipfs.infura.io', port: 5001, protocol: 'https'});
-
-                // Upload the file to IPFS
-                const { cid } = await client.add(file);
-
-                // Store the CID in the smart-contract
-                console.log('Selected file CID:', cid.toString());
-            } catch (error) {
-                console.error('Error uploading file to IPFS:', error);
-            }
-=======
 
 export class ImageUploadComponent {
     async processFile(imageInput: any): Promise<void> {
@@ -54,10 +32,9 @@ export class ImageUploadComponent {
             const cid  = await client.uploadFile(file);
            
             // Store the CID (pass it to the smart-contract)
-            console.log('Selected file CID: https://${cid}.ipfs.w3s.link');
+            console.log(`Selected file CID: https://${cid}.ipfs.w3s.link`);
         } catch (error) {
             console.error('Error uploading file to IPFS:', error);
->>>>>>> d3ba1c52ddaa8f4be03abf52578bd8234bf62114
         }
     }
 }
